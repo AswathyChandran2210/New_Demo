357ab162d0aeaf1b6dcf88d5eb7ac292d9fb6fe7
Aswathy <aswathy.kc@thinkpalm.com>
2024-06-03 16:52:50 +0530

    New change added to Newfeature branch

--- Demo.js
+++ Demo.js
@@ -2,5 +2,11 @@ let cars=["alto","dzire","esspresso"]
 console.log(typeof cars)
 console.log(cars)
 
-
+//adding new feature
+function new_car()
+{
+    cars.push("amaze")
+    console.log(cars)
+}
+new_car
 console.log("Hello-----")
